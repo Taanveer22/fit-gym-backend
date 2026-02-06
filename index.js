18039655d029686d543146dc497ce9efe21c6837
Taanveer22 <taanveer469@gmail.com>
2026-02-07 02:11:21 +0600

    alhamdulliah post method implemented in the server

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db("gymDB");
+    const scheduleCollection = database.collection("scheduleColl");
+
+    // === post/create method ===
+    app.post("/createSchedules", async (req, res) => {
+      const data = req.body;
+      const result = await scheduleCollection.insertOne(data);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
